58f2fd44635d2251537ee148b22c572cc4352f9d
Aser <ajhubero16@gmail.com>
2024-06-05 13:08:05 +0800

    Client - Added the product categories on all product

--- client/src/pages/products/AllProducts.tsx
+++ client/src/pages/products/AllProducts.tsx
@@ -4,16 +4,24 @@ import { IProduct } from "../../interfaces/productInterfaces";
 import ProductCard from "../../components/cards/ProductCard";
 import ReactPaginate from "react-paginate";
 import { useSearchParams } from "react-router-dom";
+import productCategories from "../../data/productCategories";
+import Button from "../../components/common/Button";
 
 function AllProducts() {
-  const { data } = useGetAllProductsQuery("");
   const [searchParams, setSearchParams] = useSearchParams({
     page: "1",
-    productFilter: "",
+    productFilter: "electronics",
   });
 
   const page = searchParams.get("page");
-  const productFilter = searchParams.get("productSearch");
+  const productFilter = searchParams.get("productFilter");
+
+  const productParams = {
+    page,
+    productFilter,
+  };
+
+  const { refetch, data } = useGetAllProductsQuery("");
 
   const products = data as IProduct[];
 
@@ -28,7 +36,6 @@ function AllProducts() {
     });
   };
 
-  console.log(page);
   const handlePageChange = (page: number) => {
     const realPage = page + 1;
     setSearchParams(
@@ -39,14 +46,73 @@ function AllProducts() {
       { replace: true }
     );
   };
+
+  const handleCategoryChange = (category: string) => {
+    setSearchParams(
+      (prev) => {
+        prev.set("productFilter", category);
+        return prev;
+      },
+      { replace: true }
+    );
+  };
+
+  const handleApplyFilter = () => {
+    console.log(page, productFilter);
+  };
+
+  const renderProductCategories = productCategories.map((category) => {
+    const isActive = productFilter === category.value;
+    return (
+      <div className="flex items-center gap-x-2 mb-3" key={category.id}>
+        <input
+          type="radio"
+          name="category"
+          value={category.value}
+          id={`radio-${category.id}`}
+          className="hidden"
+          checked={isActive}
+          onChange={() => handleCategoryChange(category.value)}
+        />
+        <label
+          htmlFor={`radio-${category.id}`}
+          className="flex items-center cursor-pointer"
+        >
+          <span
+            className={`w-4 h-4 inline-block mr-2 border  rounded-full ${
+              isActive ? "bg-mainColor border-mainColor" : "border-textColor"
+            }`}
+          ></span>
+          <p
+            className={`text-textColor font-kanit text-xl ${
+              isActive ? "text-activeColor" : ""
+            }`}
+          >
+            {category.name}
+          </p>
+        </label>
+      </div>
+    );
+  });
+
   return (
     <div className="py-20 bg-bgColor">
       <div className="container mx-auto">
-        <div className="flex">
-          <div className="w-1/5"></div>
-          <div className="w-4/5">
-            <div className="grid grid-cols-4 gap-7 mb-5">
-              {" "}
+        <div className="flex gap-x-10">
+          <div className="w-1/5 ">
+            <div className="sticky top-0 mt-5 bg-bgColorHover rounded-md px-3 py-5">
+              <h1 className="font-kanit text-3xl mb-5 text-mainColor">
+                Categories
+              </h1>
+              {renderProductCategories}
+
+              <div className="">
+                <Button onClick={handleApplyFilter}>Apply Filter</Button>
+              </div>
+            </div>
+          </div>
+          <div className="w-4/5 ">
+            <div className="grid grid-cols-4 gap-7 mb-5 pt-5">
               {renderProducts()}
             </div>
             <ReactPaginate
--- client/src/store/apis/productsApi.ts
+++ client/src/store/apis/productsApi.ts
@@ -24,10 +24,11 @@ const productsApi = createApi({
       }),
 
       getAllProducts: builder.query({
-        query: () => {
+        query: (params) => {
           return {
             url: "/getAllProducts",
             method: "GET",
+            params,
           };
         },
       }),
